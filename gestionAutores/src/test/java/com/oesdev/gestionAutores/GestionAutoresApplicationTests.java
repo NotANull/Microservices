d792102bb105e7c118c4df1a933295a3f88bd867
Omar Salum <o.e.salum@gmail.com>
2024-08-19 23:11:42 -0300

    API gestion autores

---
+++ gestionAutores/src/test/java/com/oesdev/gestionAutores/GestionAutoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oesdev.gestionAutores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAutoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
