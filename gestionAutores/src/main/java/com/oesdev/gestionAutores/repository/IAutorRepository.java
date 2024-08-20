d3cddd8981b0075f3add73b26a34146549c67632
Omar Salum <o.e.salum@gmail.com>
2024-08-19 23:19:17 -0300

    update folders

--- gestionAutores/src/main/java/com/oesdev/gestionAutores/repository/IAutorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oesdev.gestionAutores.repository;
-
-import com.oesdev.gestionAutores.entity.Autor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IAutorRepository extends JpaRepository<Autor, Long> {
-}
