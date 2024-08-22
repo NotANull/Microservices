023650f835ac34878510ff0c871387d70bfd6201
Omar Salum <o.e.salum@gmail.com>
2024-08-21 23:45:58 -0300

    Controlador agregado

---
+++ Ejercicio1/gestionLibros/src/main/java/com/oesdev/gestionLibros/controller/BookController.java
@@ -0,0 +1,27 @@
+package com.oesdev.gestionLibros.controller;
+
+import com.oesdev.gestionLibros.dto.request.BookDtoRequest;
+import com.oesdev.gestionLibros.dto.response.AuthorDtoResponse;
+import com.oesdev.gestionLibros.service.IBookService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/libros")
+public class BookController {
+
+    private IBookService service;
+
+    public BookController(IBookService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/create")
+    public List<AuthorDtoResponse> createBook(@RequestBody BookDtoRequest dto) {
+        return this.service.create(dto);
+    }
+}
