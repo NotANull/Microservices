617b28589939fedd99ec68ec53fff78d0b31030d
Omar Salum <o.e.salum@gmail.com>
2024-08-23 12:27:33 -0300

    Annotation en la clase

--- Gestion_libros_autores/gestionLibros/src/main/java/com/oesdev/gestionLibros/entity/Author.java
+++ Gestion_libros_autores/gestionLibros/src/main/java/com/oesdev/gestionLibros/entity/Author.java
@@ -1,5 +1,6 @@
 package com.oesdev.gestionLibros.entity;
 
+import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Embeddable
 public class Author {
 
     private String fullName;
