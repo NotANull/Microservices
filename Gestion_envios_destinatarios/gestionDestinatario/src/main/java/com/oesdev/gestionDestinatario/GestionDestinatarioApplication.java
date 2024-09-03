4a46a6ebd7d248f213652c623073d7374063fc8f
Omar Salum <o.e.salum@gmail.com>
2024-09-02 22:49:22 -0300

    gestion_envios_destinatarios

---
+++ Gestion_envios_destinatarios/gestionDestinatario/src/main/java/com/oesdev/gestionDestinatario/GestionDestinatarioApplication.java
@@ -0,0 +1,13 @@
+package com.oesdev.gestionDestinatario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDestinatarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDestinatarioApplication.class, args);
+	}
+
+}
---
+++ Gestion_envios_destinatarios/gestionDestinatario/src/test/java/com/oesdev/gestionDestinatario/GestionDestinatarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oesdev.gestionDestinatario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDestinatarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
